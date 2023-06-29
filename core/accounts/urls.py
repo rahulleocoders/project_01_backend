3542146571beff66ca596540e8e2f3480894fbae
YagneshDekate1301 <yagnesh@leocoders.com>
2023-06-29 18:55:54 +0530

    Update

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('botroleapi/',BotRoleApi.as_view(),name='botroleapi'),
     path('deleteuserbot/<int:id>/',DeleteUserBot.as_view(),name='deleteuserbot'),
     path('bulk-upload/',BulkInvitationAPI.as_view(),name='bulk_upload'),
+    path('create-api/',ApiSettingAPI.as_view(),name='create_api'),
+    path('delete-api/<int:id>',ApiSettingAPI.as_view(),name='delete_api'),
 
 ]
