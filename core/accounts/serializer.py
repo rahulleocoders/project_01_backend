05abfeb21123e01be81d5700ba4bb0cdf08b9000
neha-0411 <neha@leocoders.com>
2023-06-30 11:02:02 +0530

    update

--- core/accounts/serializer.py
+++ core/accounts/serializer.py
@@ -5,3 +5,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id','first_name','last_name','email','company_name','country_code','contact','is_contact_verfication','is_email_verfication']
+
+# class BotRoleSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = BotRole
+#         fields = "__all__"
+
