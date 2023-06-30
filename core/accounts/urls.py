07699237c86cf38166f6ef3355bbdcf2599b025f
YagneshDekate1301 <yagnesh@leocoders.com>
2023-06-30 11:42:38 +0530

    Update

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -7,14 +7,11 @@ urlpatterns = [
     path('register/<int:id>',RegistrationAPI.as_view(),name='register'),
     path('changepassword/<int:id>',ChangePassword.as_view(),name='changepassword'),
  
+    path('botroleapi/',BotRoleApi.as_view(),name='botroleapi'), # post
+    path('botroleapi/<int:id>/',BotRoleApi.as_view(),name='botroleapi'),  #put get delete
 
-
-    path('botroleapi/',BotRoleApi.as_view(),name='botroleapi'),
-    # path('deleteuserbot/<int:id>/',DeleteUserBot.as_view(),name='deleteuserbot'),
     path('bulk-upload/',BulkInvitationAPI.as_view(),name='bulk_upload'),
     path('create-api/',ApiSettingAPI.as_view(),name='create_api'),
     path('delete-api/<int:id>',ApiSettingAPI.as_view(),name='delete_api'),
-    path('botroleapi/<int:id>/',BotRoleApi.as_view(),name='botroleapi'),
-   
 
 ]
